b00605c142a4f5c7a943ce0e8186f54aa3ea9479
DanarVirkan <62247909+DanarVirkan@users.noreply.github.com>
2022-12-20 18:45:30 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LoginPage from './page/LoginPage.js';
+import './style.css';
+
+export default function App() {
+  return <LoginPage />;
+}
---
+++ src/page/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function LoginPage() {
+  return (
+    <div>
+      <h1>LOGO</h1>
+      <div>
+        <label htmlFor="">Email</label>
+        <input type="text" />
+        <label htmlFor="">Password</label>
+        <input type="password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+export default LoginPage;
